6600f0b0695aad53d72b3e55517794419ab5a83c
danielcontreras205 <danielcontreras205@gmail.com>
2024-12-04 08:51:15 -0500

    se actualiza permisos de CORS

--- src/main/java/com/appstra/users/config/CorsConfig.java
+++ src/main/java/com/appstra/users/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        //corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
