6145626e3f3d784badbe3302b21416e0a208a6e8
danielcontreras205 <danielcontreras205@gmail.com>
2025-01-03 13:49:45 -0500

    se coloca el RoleId a usuario

--- src/main/java/com/appstra/users/entity/User.java
+++ src/main/java/com/appstra/users/entity/User.java
@@ -3,6 +3,7 @@ package com.appstra.users.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.Comment;
 
 import java.sql.Timestamp;
 
@@ -15,20 +16,31 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Integer userId;
+
     @Column(name = "USER_USER")
     private String userUser;
+
     @Column(name = "USER_PASSWORD")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String userPassword;
+
     @ManyToOne
     @JoinColumn(name = "STATE_ID", referencedColumnName = "STAT_ID")
     private State state;
+
+    @Column(name = "ROLE_ID")
+    @Comment("ID del rol asociado al usuario, base de datos de compañia")
+    private Integer roleId;
+
     @Column(name = "USER_CREATION_DATE")
     private Timestamp userCreationDate;
+
     @Column(name = "USER_EDIT_DATE")
     private Timestamp userEditDate;
+
     @Column(name = "USER_EDIT_USER_ID")
     private Integer userEditUserID;
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Person person;
